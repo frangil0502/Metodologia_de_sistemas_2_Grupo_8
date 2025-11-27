22eb561ec389d746297a91c8f5be3115026a6f2a
Anawello <mider2014@gmail.com>
2025-11-27 09:46:06 -0300

    Arregle problema en el refactoring

--- refactoring/Personaje.java
+++ refactoring/Personaje.java
@@ -1,5 +1,7 @@
 package refactoring;
 
+import refactoring.TipoHabilidad;
+
 
 // Personaje
 
@@ -24,6 +26,28 @@ public class Personaje {
     }
 
     public TipoHabilidad getTipoHabilidad() {
-        return tipoHabilidad;
+        return this.tipoHabilidad;
+    }
+
+    public Personaje personajeConMasDaño(Personaje[] personajes) {
+
+        Personaje temp = null;
+        double max = 0;
+
+        for (Personaje p : personajes) {
+            double daño = p.getTipoHabilidad().calcularDaño(p.getDaño());
+            if (daño > max) {
+                temp = p;
+                max = daño;
+            }
+        }
+
+        return temp;
+    }
+
+    public void imprimirInfo() {
+        System.out.println(this.nombre + " tiene como daño " + this.daño);
+        System.out.println(tipoHabilidad.descripcion());
+        System.out.println(tipoHabilidad.efecto());
     }
 }
--- refactoring/Videojuego.java
+++ refactoring/Videojuego.java
@@ -13,30 +13,4 @@ public class Videojuego {
     public Videojuego(List<Personaje> personajes) {
         this.personajes = personajes;
     }
-
-    public Personaje personajeConMasDaño() {
-
-        Personaje temp = null;
-        double max = 0;
-
-        for (Personaje p : personajes) {
-            double daño = p.getTipoHabilidad().calcularDaño(p.getDaño());
-            if (daño > max) {
-                temp = p;
-                max = daño;
-            }
-        }
-
-        return temp;
-    }
-
-    public void imprimirInfo(Personaje p) {
-
-        System.out.println(p.getNombre() + " tiene como daño " + p.getDaño());
-
-        TipoHabilidad h = p.getTipoHabilidad();
-
-        System.out.println(h.descripcion());
-        System.out.println(h.efecto());
-    }
 }
